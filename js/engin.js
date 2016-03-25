9c23e5ae139f67c21ab97d41ec0dffab301d9ee1
scanick <scanick@yandex.ru>
2016-03-25 17:40:15 +0300

    Добавлена анимация, перерисовка при изменении размеров окна

--- js/engin.js
+++ js/engin.js
@@ -4,13 +4,14 @@
 **===============================================================================================================*/
 	var ctx;
 
-	var timeout = 1000, fps = 35, dt = timeout/fps;// число кадров в секунду
+	var timeout = 1000, fps = 15, dt = timeout/fps;// число кадров в секунду
 	var gDate = new Date(); //дата/время
 	
 
 	var Colors = {
 		ground: '#e8e8e8',//Цвет фона
 		fontMonth: '#000000',//цвет шрифта
+		scoreboard: ['#000000', '#aaaaaa'],
 	};
 
 	var field = {
@@ -18,12 +19,16 @@
 		fHeight: window.innerHeight,	// Высота поля
 		fontMonth: '15px Comic Sans MS',//Шрифт
 		center: [0, 0],					// ХУ центра поля
-		
+		rExternal: 0,				// Внешний радиус календаря
+		rClock: 0,						//Внешний радиус часов
 		init: function() {
-			this.fWidth *=1; // Ширина поля
-			this.fHeight *=1; // Высота поля
-			this.center[0] = this.fWidth/2; // Х центра поля
-			this.center[1] = this.fHeight/2;// У центра поля
+			this.fWidth = window.innerWidth; 	// Ширина поля
+			this.fHeight = window.innerHeight; 	// Высота поля
+			this.center[0] = this.fWidth/2; 	// Х центра поля
+			this.center[1] = this.fHeight/2;	// У центра поля
+			this.rExternal = this.fWidth > this.fHeight ? 2*this.fHeight/5 : 2*this.fWidth/5; // Внешний радиус календаряthis.rExternal = this.fWidth > this.fHeight ? 2*this.fHeight/5 : 2*this.fWidth/5; // Внешний радиус календаря
+			this.rClock = this.rExternal/1.9; // Внешний радиус календаря
+			
 		},
 	};
 
@@ -40,12 +45,18 @@
 		* public function
 		**========================================================================================================*/
 		this.init = function(){
-			hours   = gDate.getHours();
+			/* hours   = gDate.getHours();
 			minutes = gDate.getMinutes();
 			seconds = gDate.getSeconds();
-			console.log(hours + ':' + minutes + ':' + seconds);
+			console.log(hours + ':' + minutes + ':' + seconds); */
 		};
-		
+		this.getTime = function(){
+			hours   = new Date().getHours();
+			minutes = new Date().getMinutes();
+			seconds = new Date().getSeconds();
+			
+			return hours + ':' + minutes + ':' + seconds;
+		};
 		this.draw = function(mod){
 			draw(mod);
 		};
@@ -79,16 +90,20 @@
 		canva.height = field.fHeight;
 		ctx = canva.getContext('2d');
 	
-		clock = new classClock().init();
+		clock = new classClock();
+		clock.init();
 	
 		clear();
-		console.log(field.fWidth + 'x' + field.fHeight);
+		draw();
+		//console.log(field.fWidth + 'x' + field.fHeight);
+		animation();
 	};
 
 	self.events = function(e, data){
 		switch(e){
 			case 'resizeWindow':
-				field.fWidth = window.innerWidth; field.fHeight = window.innerHeight;
+				field.init();
+				//field.fWidth = window.innerWidth; field.fHeight = window.innerHeight;
 				console.log(field.fWidth + 'x' + field.fHeight);
 				break;
 			case 'mousemove':
@@ -112,15 +127,35 @@
 	};
 
 	draw = function(obj){
+		ctx.strokeText(clock.getTime(), field.center[0]*5/4, field.center[1]/2);
 		
-		switch(obj.getType()){
+		//createRadialGradient(X_star, Y_start, Radius_inner, X_end, Y_end, Radius_external)
+		var grdr = ctx.createRadialGradient(field.center[0],field.center[1], 0, field.center[0], field.center[1], field.rClock);
+		grdr.addColorStop( 0, Colors.scoreboard[0] );//Color_start
+		grdr.addColorStop( 1, Colors.scoreboard[1] );//Color_end
+		ctx.fillStyle = grdr; // меняем цвет
+		
+		ctx.beginPath();//context.arc(x,y,r,sAngle,eAngle,counterclockwise);
+			ctx.arc(field.center[0], field.center[1], field.rClock, 0, 2*Math.PI);
+		ctx.closePath();
+		ctx.fill();
+		/* switch(obj.getType()){
 			case '':
 				break;
 			case ' ':
 				break;
 			default: break;
-		}
+		} */
 		
 	};
 
+	function animation() {
+		
+		setTimeout(function(){
+			requestAnimationFrame(animation);
+			clear();
+			draw();
+			//console.log('anim');
+		}, dt);
+	}
 })( this.engin = {});
