b1ccbd2de6d9b808ddf5380f1b68c4cb4c31058d
scanick <scanick@yandex.ru>
2016-03-27 23:19:51 +0400

    Прорисовка часовой спирали

--- js/engin.js
+++ js/engin.js
@@ -6,20 +6,22 @@
 
 	var timeout = 1000, fps = 15, dt = timeout/fps;// число кадров в секунду
 	var gDate = new Date(); //дата/время
-	
+
 
 	var Colors = {
 		ground: '#e8e8e8',//Цвет фона
 		fontMonth: '#000000',//цвет шрифта
 		scoreboard: ['#000000', '#aaaaaa'],
+		hours: ['#110000', '#ff99aa', '#881122', '#440011'],
 	};
 
 	var field = {
 		fWidth: window.innerWidth,		// Ширина поля
 		fHeight: window.innerHeight,	// Высота поля
-		fontMonth: '15px Comic Sans MS',//Шрифт
+		fontMonth: '15px Comic Sans MS',// Шрифт
 		center: [0, 0],					// ХУ центра поля
-		rExternal: 0,				// Внешний радиус календаря
+		startAngle: -Math.PI/2,			// Стартовый угол
+		rExternal: 0,					// Внешний радиус календаря
 		rClock: 0,						//Внешний радиус часов
 		init: function() {
 			this.fWidth = window.innerWidth; 	// Ширина поля
@@ -41,6 +43,9 @@
 		**=======================================================================================================*/
 		var self = this;
 		var hours, minutes, seconds;
+		var hSpiral = [];
+		var hAngle = Math.PI/12;
+		
 		/*========================================================================================================
 		* public function
 		**========================================================================================================*/
@@ -149,12 +154,51 @@
 		
 	};
 
-	function animation() {
+	drawHourse = function(){
+		let drH = field.rClock/(4*12);	 //За 12 часов радиус уменьшится на 4
+		let drM = drH/60;				 // За 60 минут радиус часовой спирали ещё уменьшится
+		let daH = Math.PI/6;			 //угол одного часа спирали
+		let daM = daH/60;				 //угол минуты часовой спирали
+		for(let i = 0; i < new Date().getHours(); i++){//new Date().getHours()
+			for(let j = 0; j < 60; j++){
+				let cos = Math.cos(field.startAngle + i*daH + j*daM);
+				let sin = Math.sin(field.startAngle + i*daH + j*daM);
+				let x1 = field.center[0] + (field.rClock - 8 - i*drH - j*drM ) * cos;
+				let y1 = field.center[1] + (field.rClock - 8 - i*drH - j*drM ) * sin;
+				let x2 = field.center[0] + (field.rClock     - i*drH - j*drM ) * cos;
+				let y2 = field.center[1] + (field.rClock     - i*drH - j*drM ) * sin;
+				
+				var grd = ctx.createRadialGradient(x1, y1, 1, x2, y2, 2);
+				grd.addColorStop( 0, Colors.hours[0] );//Color_start
+				grd.addColorStop( 0.4, Colors.hours[1] );//Color_end
+				grd.addColorStop( 0.6, Colors.hours[2] );//Color_end
+				grd.addColorStop( 1, Colors.hours[3] );//Color_end
+				ctx.strokeStyle = grd;
+				
+				ctx.shadowBlur=20;
+				ctx.shadowColor="black";
+				
+				ctx.beginPath();
+					ctx.moveTo( x1, y1 );
+					ctx.lineTo( x2, y2 );
+				ctx.closePath();
+				ctx.lineWidth = 2;
+				ctx.stroke();
+			}
+			/* ctx.beginPath();//context.arc(x,y,r,sAngle,eAngle,counterclockwise);
+				ctx.arc(x, y, 4, 0, 2*Math.PI);
+			ctx.closePath();
+			ctx.fill(); */
+		}
 		
+	};
+
+	function animation(){
 		setTimeout(function(){
 			requestAnimationFrame(animation);
 			clear();
 			draw();
+			drawHourse();
 			//console.log('anim');
 		}, dt);
 	}
